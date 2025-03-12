2abf737b06ab094ab8dd387423c089209327824c
longtrieu2000 <157421218+longtrieu2000@users.noreply.github.com>
2025-03-12 18:10:56 +0700

    4rd commit

--- userdata/maven_setup.sh
+++ userdata/maven_setup.sh
@@ -2,6 +2,7 @@
 sudo apt update
 sudo apt install -y wget unzip tar 
 mkdir /tmp/maven
+mkdir /opt/maven
 cd /tmp/maven
 wget https://downloads.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
 sudo tar -xvzf apache-maven-*.tar.gz -C 
